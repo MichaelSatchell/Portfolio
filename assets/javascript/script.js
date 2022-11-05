a74c51360f97b1597aa21d0de415cbf35818eb33
Michael Satchell Jr <michaelsatchelljr.@Michaels-MacBook-Pro.local>
2022-11-05 08:57:15 -0700

    added a reset button to the contact form/ added a hover effect and Task Master image cover to the project cards/ added pr oject section title

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,66 +1,27 @@
-const hideWords = () =>
-    onmouseover
 
-
-
-op Nav bar gloabl variables from HTML elements
-
-const navBarTop = document.getElementsByClassName('li');
-
-const navBarTopLinks = () => {
-
+document.getElementById('submitButton').onclick = function() {myEmail()};
+const myEmail = () => {
+    document.getElementById('submitButton').innerHTML = "You Clicked Me";
 }
 
 
-navBarTop.addEventListener('click', clearNames)
 
-resumeLink = () => {
-    document.getElementById('resume-link').href= "https://www.cnn.com/";
+// @media (max-width: 767px) {
+//     .image-container {
+//         display: none;
+//     }
+// }
+
+
+sendEmail = () +> {
+    const mail = 'michaelsatchelljr@outlook.com'
+}
+
+
+inputNeeded = () +> {
+    const empty = document.getElementById('firstName');
+    if(empty == null){
+        alert("Please fill in all input fields");
+    }
     
 }
-resumeLink.addEventListener('click');
-
-// Global variables from HTML elements
-const randomNameButton = document.getElementById('random-names-button');
-const clearButton = document.getElementById('clear-list');
-const ol = document.getElementById('names-list');
-
-
-// Function to generate random names
-const generateNames = () => {
-    ol.innerHTML = '';
-
-    //Get number value form input
-    let numOfNames = document.getElementById('number-of-names').value;
-
-    // Get text box from HTML
-    let textBox = document.getElementById('text-box');
-    //Get names from text box and separate them at each comma and put them in an array
-    let namesArr = textBox.value.split(',');
-
-    //Select random name form namesArray
-    if(numOfNames > namesArr.length) {
-        alert('Please enter a number less than the total names')
-    } else {
-        for(let i = 0; i < numOfNames; i++) {
-            let randomName = namesArr[Math.floor(Math.random() * namesArr.length)];
-            let li = document.createElement('li')
-            li.innerHTML = randomName
-            ol.appendChild(li)
-        }
-    }
-
-    //Build names list in browser
-}
-
-
-//Clear function
-const clearNames = () => {
-    ol.innerHTML = '';
-}
-
-
-// Button click events
-
-randomNameButton.onclick = generateNames;
-clearButton.addEventListener('click', clearNames)
